cd91a02be8c1b59fd1fa4c5d9bb1a817168ca7bc
Kimsangwon <kimsw509@g2e.co.kr>
2020-12-03 12:49:40 +0900

    20201203 input

---
+++ chapter10_01.py
@@ -0,0 +1,19 @@
+#chapter05-2
+#파이썬 사용자 입력
+# Input 사용법
+# 기본 타입(str)
+
+# 예제1
+#name = input("Enter Your Name: ")
+#grade = input("Enter Your Grade: ")
+#company = input("Enter Your company name: ")
+
+#print(name, grade, company)
+
+#예제 2
+#number1 = int(input("enter number1: "))
+#number2 = int(input("enter number1: "))
+#print(number1*number2)
+
+#예제 3
+print("firstName - {0},Lastname - {1}".format(input("Enter firstname: "), input("enter lastname: ")))
